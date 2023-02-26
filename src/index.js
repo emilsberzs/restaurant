825a235c7e3d63aedb392110c7aa9e24f87bd730
Emils <emilsberzs@hotmail.com>
2023-02-26 10:59:51 +0000

    Dynamic population starting to take shape-NAV done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './styles.css';
 
 console.log('test test test')
+
+function firstLoad() {
+    //Initialize 'CONTENT' DIV
+    const content = document.getElementById('content')
+    const header = document.createElement('header')
+    const navbar = document.createElement('nav')
+    //SET NAVBAR LINKS
+    const home = document.createElement('a')
+    home.setAttribute('href', '#')
+    home.innerHTML = 'HOME'
+    const menu = document.createElement('a')
+    menu.setAttribute('href', '#')
+    menu.innerHTML = 'MENU'
+    const contact = document.createElement('a')
+    contact.setAttribute('href', '#')
+    contact.innerHTML = 'CONTACT US'
+
+
+    navbar.append(home, menu, contact)
+    content.appendChild(header);
+    header.appendChild(navbar)
+}
+
+
+firstLoad()
