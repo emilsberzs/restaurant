7f36f25d741b2042b09c68232dca06c45ef3985d
Emils <emilsberzs@hotmail.com>
2023-02-26 12:37:44 +0000

    Also seperated Header builder in seperate module and imported into index, trying to keep index as clean as possible

---
+++ src/header.js
@@ -0,0 +1,22 @@
+export const loadHeader = function () {
+    const content = document.getElementById('content')
+    const header = document.createElement('header')
+    const navbar = document.createElement('nav')
+    //HEADER
+    const mainTitle = document.createElement('h1')
+    mainTitle.innerHTML = 'Restaurante'
+    //NAVBAR
+    const home = document.createElement('a')
+    home.setAttribute('href', '#')
+    home.innerHTML = 'HOME'
+    const menu = document.createElement('a')
+    menu.setAttribute('href', '#')
+    menu.innerHTML = 'MENU'
+    const contact = document.createElement('a')
+    contact.setAttribute('href', '#')
+    contact.innerHTML = 'CONTACT US'
+    navbar.append(home, menu, contact)
+    header.appendChild(mainTitle);
+    content.appendChild(header);
+    content.appendChild(navbar);
+}
