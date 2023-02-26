2b71189707dd6c8757a9196ebbbd35ae517a6bdc
Emils <emilsberzs@hotmail.com>
2023-02-26 12:34:41 +0000

    Exported homePage building function as seperate module and imprted into index.js. Now on to make external modules for Menu and Contact Us

---
+++ src/home.js
@@ -0,0 +1,25 @@
+export const homePage = function () {
+    //Initialize 'CONTENT' DIV
+    const content = document.getElementById('content')
+    //MAIN
+    const main = document.createElement('main')
+    const usp = document.createElement('div')
+    usp.setAttribute('id', 'usp')
+    const usp1 = document.createElement('div')
+    usp1.setAttribute('id', 'usp1')
+    usp1.innerHTML = 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.'
+    const usp2 = document.createElement('div')
+    usp2.setAttribute('id', 'usp2')
+    usp2.innerHTML = 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.'
+    const usp3 = document.createElement('div')
+    usp3.setAttribute('id', 'usp3')
+    usp3.innerHTML = 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.'
+    const usp_image = document.createElement('img')
+    usp_image.setAttribute('id', 'usp-image')
+    usp_image.setAttribute('src', '../src/image2.jpg')
+    usp_image.setAttribute('height', '200px')
+    usp.append(usp1, usp2, usp3, usp_image)
+    main.appendChild(usp)
+    //BUILD UP THE PAGE FROM CREATED ELEMENTS
+    content.appendChild(main)
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './styles.css';
+import { homePage } from './home';
 
-console.log('test test test')
 
-function firstLoad() {
-    //Initialize 'CONTENT' DIV
+//REWRITE HEADER AND NAVBAR IN SEPERATE MODEL
+const loadHeader = function () {
     const content = document.getElementById('content')
     const header = document.createElement('header')
     const navbar = document.createElement('nav')
@@ -20,32 +20,12 @@ function firstLoad() {
     const contact = document.createElement('a')
     contact.setAttribute('href', '#')
     contact.innerHTML = 'CONTACT US'
-    //MAIN
-    const main = document.createElement('main')
-    const usp = document.createElement('div')
-    usp.setAttribute('id', 'usp')
-    const usp1 = document.createElement('div')
-    usp1.setAttribute('id', 'usp1')
-    usp1.innerHTML = 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.'
-    const usp2 = document.createElement('div')
-    usp2.setAttribute('id', 'usp2')
-    usp2.innerHTML = 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.'
-    const usp3 = document.createElement('div')
-    usp3.setAttribute('id', 'usp3')
-    usp3.innerHTML = 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.'
-    const usp_image = document.createElement('img')
-    usp_image.setAttribute('id', 'usp-image')
-    usp_image.setAttribute('src', '../src/image2.jpg')
-    usp_image.setAttribute('height', '200px')
-    usp.append(usp1, usp2, usp3, usp_image)
-    main.appendChild(usp)
-    //BUILD UP THE PAGE FROM CREATED ELEMENTS
     navbar.append(home, menu, contact)
     header.appendChild(mainTitle);
     content.appendChild(header);
     content.appendChild(navbar);
-    content.appendChild(main)
 }
 
 
-firstLoad()
+loadHeader()
+homePage()
